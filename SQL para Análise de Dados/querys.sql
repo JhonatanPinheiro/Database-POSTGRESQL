5ae488986a8dfcf6053449109d1ec9ba1431b72c
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2023-04-22 15:54:39 -0300

    Usando SELECT

--- SQL para Análise de Dados/querys.sql
+++ SQL para Análise de Dados/querys.sql
@@ -4,3 +4,28 @@
 ---------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------
 
+---------------------8. SELECT---------------------
+SELECT coluna_1,coluna_1,coluna_3
+FROM schema_1.nome_da_tabela;
+
+-- Seleção de uma coluna de uma tabela
+-- Liste os e-mails dos clientes da tabela sales.customers
+SELECT email
+FROM sales.customers
+
+
+--Seleção de mais de uma coluna  de uma tabela
+--Liste os emails e nomes dos clientes da tabela sales.customers
+SELECT email, firsty_name, last_name 
+FROM sales.customers
+
+
+-- Seleçao de todas as colunas de uma table
+-- Liste todas as informações dos clientes da tabela sales.customers
+SELECT * FROM sales.customers
+
+--RESUMO
+-- 1 Comando usado para selecionar colunas de tabelas
+-- 2 Quando selecionar mais de duas coluna, elas devem ser separadas por vígula sem conter vírgula antes do comando FROM
+-- 3 Pode-se utilizar o asterisco (*) para selecionar todas as colunas da tabela
+
