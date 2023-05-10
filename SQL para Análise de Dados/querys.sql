cb5a786466eaa986de0a4cdd17e9250b40a1ed19
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2023-05-09 21:24:20 -0300

    26. Desafio

--- SQL para Análise de Dados/querys.sql
+++ SQL para Análise de Dados/querys.sql
@@ -696,5 +696,15 @@ select * from sales.customers limit 10;
 select * from temp_tables.regions limit 10;
 
 
+--------------------------------------------26. Desafio-------------------------------------
+-- EXERCÍCIOS ########################################################################
+-- (Exercício 1) Identifique quais as marcas de veículo mais visitada na tabela sales.funnel
+
+
+-- (Exercício 2) Identifique quais as lojas de veículo mais visitadas na tabela sales.funnel
+
+
+-- (Exercício 3) Identifique quantos clientes moram em cada tamanho de cidade (o porte da cidade
+-- consta na coluna "size" da tabela temp_tables.regions)
 
 
