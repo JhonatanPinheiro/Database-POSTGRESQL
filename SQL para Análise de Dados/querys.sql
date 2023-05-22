553b7bcc27f794b1a0500cf0b794c2c7a03e157b
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2023-05-22 05:17:00 -0300

    31 - EXEMPLOS (usando WITH)

--- SQL para Análise de Dados/querys.sql
+++ SQL para Análise de Dados/querys.sql
@@ -891,9 +891,54 @@ SELECT
 FROM sales.funnel AS fun
 LEFT JOIN sales.stores AS sto -- Para pegar o nome da loja
     ON fun.store_id = sto.store_id
-ORDER BY sto.store_name, fun.visit_page_date;`
+ORDER BY sto.store_name, fun.visit_page_date;
 
 -- RESUMO 
 -- (1) Servem para consultar dados de outros consultados.
 -- (2) Para que as subqueries no WHERE e no SELECT funcionem, elas devem retornar apenas um único valor
 -- (3) Não é recomendado utilizar subqueries diretamente dentro do FROM pois isso dificuta a legibilidade da query
+
+
+
+--------------------------------------------31. EXEMPLOS Tipos de Subquery-------------------------------------
+-- (Exemplos 1) Análise de recorrência dos leads
+-- Calcule o volume de visitas por dia ao site separado por 1º visita e demais vísitas
+
+WITH primeira_visita AS (
+
+    SELECT customer_id,MIN(visit_page_date) AS visit_1
+    FROM sales.funnel
+    GROUP BY customer_id
+)
+SELECT
+    fun.visit_page_date,
+    (fun.visit_page_date <> primeira_visita.visit_1) AS lead_recorrente,
+    COUNT(*)
+FROM sales.funnel AS fun
+LEFT JOIN primeira_visita
+    ON fun.customer_id = primeira_visita.customer_id
+GROUP BY fun.visit_page_date, lead_recorrente
+ORDER BY fun.visit_page_date DESC, lead_recorrente;
+
+
+-- (EXEMPLO 2 ) Análise do preço versus o preço médio
+-- Calcule, para cada visita ao site, quanto o preço do um veículo visitado pelo cliente
+-- estava acima ou baixo do preço médio dos veículos daquela marca
+-- (levar em consideração o desconto dado no veículo)
+
+WITH preco_medio AS (
+    SELECT brand, AVG(price) AS preco_medio_da_marca
+    FROM sales.products
+    GROUP BY brand
+)
+SELECT fun.visit_id,
+       fun.visit_page_date,
+       pro.brand,
+       (pro.price * (1 + fun.discount)) AS preco_final,
+       preco_medio.preco_medio_da_marca,
+       (pro.price * (1 + fun.discount)) - preco_medio.preco_medio_da_marca AS preco_vs_media
+FROM sales.funnel AS fun
+LEFT JOIN sales.products AS pro
+    ON fun.product_id = pro.product_id
+LEFT JOIN preco_medio
+    ON pro.brand = preco_medio.brand
